8eaf5c09e2634caf13fbf011f7be9a938a1f8355
Mainthan Suriyakumaran <mainthan2@gmail.com>
2025-07-19 00:59:39 +0100

    More cleanup

--- chatroom/server.py
+++ chatroom/server.py
@@ -1,17 +1,19 @@
 import asyncio
 
 class Server:
-    def __init__(self, hostname="localhost", port=40004):
+    def __init__(self, host="localhost", port=40004):
         self.clients = []
-        self.hostname = hostname
+        self.host = host
         self.port = port
 
     async def start_server(self):
-        await asyncio.start_server(self.callback, self.hostname, self.port)
+        await asyncio.start_server(
+            self.client_connected_cb, self.host, self.port)
+
         while True:
             await asyncio.sleep(1)
 
-    def callback(self, reader, writer):
+    def client_connected_cb(self, reader, writer):
         print("Hello world!")
 
 if __name__ == "__main__":
